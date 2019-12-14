e197f7970d5a3d5a83f464815e096e8beec48712
Digitallync <digitallync@Digitals-MacBook-Air-2.local>
2019-12-14 10:32:49 +0530

    students2 project

---
+++ students28/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import include,url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^home/', include('leads.urls')),
+    url(r'^do_admin_login/', include('leads.urls')),
+	url(r'^demo/',  include('leads.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^leads/', include('leads.urls')),
+    url(r'^registers/', include('leads.urls')),
+    url(r'^joinings/', include('leads.urls')),
+    url(r'^walk/', include('leads.urls')),
+    url(r'^current/', include('leads.urls')),
+    url(r'^calling/', include('leads.urls')),
+    url(r'^counselling/', include('leads.urls')),
+    url(r'^complect/', include('leads.urls')),
+    url(r'^students/', include('leads.urls')),
+
+]
